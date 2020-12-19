0d93793228b02448b46e2cca25b4661295515683
alissa1228 <68986081+alissa1228@users.noreply.github.com>
2020-12-19 20:04:04 +0900

    XmasCard

--- xmas.js
+++ xmas.js
@@ -3,8 +3,14 @@ function openIt() {
     document.getElementsByClassName("letter-open")[0].style.display = 'block'
 }
 
+function goRtan() {
+    alert("Happy New Year!");
+    window.location.href="https://youtu.be/3Uo0JAUWijM";
+}
+
 function init() {
     openIt();
+    goRtan();
 }
 
 init(); 
